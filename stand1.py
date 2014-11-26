6eb6d120fe717ff0b5f8f1a08356c74cc8df8bf6
Mike <michael.reddock@gmail.com>
2014-11-25 23:01:17 -0600

    Add up and down

--- stand1.py
+++ stand1.py
@@ -7,6 +7,14 @@ current_time = str(datetime.datetime.now())
 
 target = open(output_file, 'a')
 
+print "Standing up!"
 target.write(current_time)
+target.write("|")
+print "Hit enter when sitting down"
+wait = raw_input()
+end_time = str(datetime.datetime.now())
+target.write(end_time)
 target.write("\n")
+
+
 target.close
