78baaf8cb5015ae0e33ad6ee194b51fc3228948d
Mike <michael.reddock@gmail.com>
2014-11-26 21:39:13 -0600

    Add sound

--- stand1.py
+++ stand1.py
@@ -1,7 +1,9 @@
 from sys import argv
 import datetime
+import sys
 
 output_file = "standing.log"
+os_version = sys.platform
 
 current_time = str(datetime.datetime.now())
 
@@ -16,5 +18,12 @@ end_time = str(datetime.datetime.now())
 target.write(end_time)
 target.write("\n")
 
+# Play a sound if it is on Windows or OSX
+if os_version == "win32":
+	import winsound
+	winsound.PlaySound("C:\drum.wav", 1)
+else:
+  sys.stdout.write('\a')
+  sys.stdout.flush()
 
 target.close
